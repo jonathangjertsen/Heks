3cfd4cf9190ebcd01dbdfd1b07b10345514c115b
Jonathan Reichelt Gjertsen <jonathan.gjertsen@disruptive-technologies.com>
2019-12-25 20:56:13 +0100

    Limit the camera's movement (only minX and minY)

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public float minY = 0;
+    public float minX = 0;
+
     float updateCoeffX = 0.1f;
     float updateCoeffY = 0.1f;
 
     public Transform player;
 
+    float weightedDistanceX(float targetX)
+    {
+        return updateCoeffX * (targetX - transform.position.x);
+    }
+
+    float weightedDistanceY(float targetY)
+    {
+        return updateCoeffY * (targetY - transform.position.y);
+    }
+
     void FixedUpdate()
     {
-        float diffX = updateCoeffX * (player.position.x - transform.position.x);
-        float diffY = updateCoeffY * (player.position.y - transform.position.y);
+        float diffX;
+        float diffY;
+
+        if (player.position.x <= minX)
+        {
+            diffX = weightedDistanceX(minX);
+        }
+        else
+        {
+            diffX = weightedDistanceX(player.position.x);
+        }
+
+        if (player.position.y <= minY)
+        {
+            diffY = weightedDistanceY(minY);
+        }
+        else
+        {
+            diffY = weightedDistanceY(player.position.y);
+        }
+
         transform.position += new Vector3(diffX, diffY, 0);
     }
 }
