200f0cfc88b12a0ae56847b81efa04351cf8e3b4
Jonathan Reichelt Gjertsen <jonathan.gjertsen@disruptive-technologies.com>
2019-12-30 19:19:11 +0100

    Add first working unit test

---
+++ Assets/Tests/Basic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TestSuite
+{
+    [UnityTest]
+    public IEnumerator TestsCanRun()
+    {
+        SceneManager.LoadScene("Levels/Testing");
+        yield return null;
+    }
+}
