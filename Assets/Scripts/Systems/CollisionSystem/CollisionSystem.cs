9ff8ddf46983848a880b36157804ffb27f4de409
Jonathan Reichelt Gjertsen <jonathan.gjertsen@disruptive-technologies.com>
2020-01-07 19:48:02 +0100

    Add collision debouncing

--- Assets/Scripts/Systems/CollisionSystem/CollisionSystem.cs
+++ Assets/Scripts/Systems/CollisionSystem/CollisionSystem.cs
@@ -1,11 +1,45 @@
-﻿public static class CollisionSystem
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CollisionSystem
 {
     public static float minDefense = 0.01f;
+    public static float minTimeBetweenCollisionsOfSameObject = 0.5f;
+    public static Dictionary<Tuple<ICollisionSystemParticipator, ICollisionSystemParticipator>, float> lastCollision;
+
+    static CollisionSystem()
+    {
+        lastCollision = new Dictionary<Tuple<ICollisionSystemParticipator, ICollisionSystemParticipator>, float>();
+    }
 
     public static void RegisterCollision(ICollisionSystemParticipator first, ICollisionSystemParticipator other)
     {
+        if (!DebounceCollision(first, other))
+        {
+            return;
+        }
         CollisionDamage(first, other);
         CollisionStatusEffect(first, other);
+    }
+
+    private static bool DebounceCollision(ICollisionSystemParticipator first, ICollisionSystemParticipator other)
+    {
+        float time = Time.fixedTime;
+        var tuple = new Tuple<ICollisionSystemParticipator, ICollisionSystemParticipator>(first, other);
+        if (lastCollision.TryGetValue(tuple, out float value))
+        {
+            if (time - value < minTimeBetweenCollisionsOfSameObject)
+            {
+                return false;
+            }
+            lastCollision[tuple] = time;
+        }
+        else
+        {
+            lastCollision.Add(tuple, time);
+        }
+        return true;
     }
 
     private static void CollisionDamage(ICollisionSystemParticipator first, ICollisionSystemParticipator other)
